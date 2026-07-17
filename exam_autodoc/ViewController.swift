7fb5f9046f19380e942cd75998465f251ba9a528
Pavel Krivtsov <krivcov.pavel@gmail.ru>
2026-07-17 20:47:52 +0300

    Initial Commit

---
+++ exam_autodoc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exam_autodoc
+//
+//  Created by Павел Кривцов on 17.07.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
